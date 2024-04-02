b305ebab9adf579e23a34e66dfefa46199bb393e
Danila Muravyev <mixidwork@gmail.com>
2024-04-02 10:28:55 +0300

    settings main app fix

--- recipesite/settings.py
+++ recipesite/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'recipes',
-    'main',
     'users',
 ]
 
@@ -122,6 +121,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = 'auth.User'
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
