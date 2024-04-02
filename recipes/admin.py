05037fa33829c9e77bfe7e4cdf43bed20b19920c
Danila Muravyev <mixidwork@gmail.com>
2024-04-02 10:30:15 +0300

    admin panel settings

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from recipes import models
+
 # Register your models here.
+admin.site.register(models.Recipe)
+admin.site.register(models.Ingredient)
+admin.site.register(models.RecipeCategory)
