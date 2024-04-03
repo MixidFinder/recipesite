1dc0f6fb5623cc2a64da0be7d50cf8af04483e63
Danila Muravyev <mixidwork@gmail.com>
2024-04-03 11:31:24 +0300

    settings redirection set

--- recipesite/settings.py
+++ recipesite/settings.py
@@ -123,7 +123,8 @@ AUTH_PASSWORD_VALIDATORS = [
 
 AUTH_USER_MODEL = 'auth.User'
 LOGIN_URL = 'login'
-LOGIN_REDIRECT_URL = ''
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
