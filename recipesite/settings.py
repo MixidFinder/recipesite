9ced4659bc67a3c8c536431e55611ab27e6410f5
Danila Muravyev <mixidwork@gmail.com>
2024-04-05 13:44:01 +0300

    ready for deploy

--- recipesite/settings.py
+++ recipesite/settings.py
@@ -21,12 +21,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h*&!5^vn&qhah_@)kl@(p9ez0^cn_&%5&^yky9p1-x%dqo*plj'
+SECRET_KEY = os.getenv('SECRET_KEY')
+
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'mixfingb.pythonenywhere.com',
+]
 
 
 # Application definition
@@ -79,8 +85,15 @@ WSGI_APPLICATION = 'recipesite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mixfingb$default',
+        'USER': 'mixfingb',
+        'PASSWORD': os.getenv('MYSQL_PASSWORD'),
+        'HOST': 'mixfingb.mysql.pythonanywhere-services.com',
+        'OPTIONS': {
+            'init_command': "SET NAMES 'utf8mb4';SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+        },
     }
 }
 
@@ -143,6 +156,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
