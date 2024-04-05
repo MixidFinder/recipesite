94d0fc7a376988194830c806b94f80933a34710a
Danila Muravyev <mixidwork@gmail.com>
2024-04-05 17:32:29 +0300

    admin panel setting

--- recipes/admin.py
+++ recipes/admin.py
@@ -2,7 +2,23 @@ from django.contrib import admin
 
 from recipes import models
 
+
+class CategorytAdmin(admin.ModelAdmin):
+    ordering = ['name']
+    search_fields = ['name']
+
+
+class IngredientAdmin(admin.ModelAdmin):
+    ordering = ['name']
+    search_fields = ['name']
+
+
+class RecipeAdmin(admin.ModelAdmin):
+    ordering = ['creation_date']
+    search_fields = ['name', 'author']
+
+
 # Register your models here.
-admin.site.register(models.Recipe)
-admin.site.register(models.Ingredient)
-admin.site.register(models.Category)
+admin.site.register(models.Recipe, RecipeAdmin)
+admin.site.register(models.Ingredient, IngredientAdmin)
+admin.site.register(models.Category, CategorytAdmin)
